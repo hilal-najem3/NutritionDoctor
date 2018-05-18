0ef4ef9ee2559210185f972f0ca9bee79123cb81
hilal-najem <hilal123work@gmail.com>
2018-05-18 17:40:21 +0300

    Start Staging

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             FoodCategoriesTableSeeder::class,
-            FoodTableSeeder::class,
         ]);
     }
 }
