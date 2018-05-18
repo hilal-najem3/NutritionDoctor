f6a941f1c85a01d37cfb750f62683e93b34da928
hilal-najem <hilal123work@gmail.com>
2018-05-18 17:41:26 +0300

    Start develop

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call([
-            UsersTableSeeder::class,
             FoodCategoriesTableSeeder::class,
             FoodTableSeeder::class,
         ]);
