6438cad67389b132baff87439c64a74f724207a1
hilal-najem <hilal123work@gmail.com>
2018-05-18 17:37:41 +0300

    Update string lenght and Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             FoodCategoriesTableSeeder::class,
             FoodTableSeeder::class,
+            NewUsersTableSeeder::class,
         ]);
     }
 }
